bc0f7dd9a25bd33d7b20b91be715e39870ae4eab
Eduardo Henke <eduardo_henke@hotmail.com>
2018-10-06 18:13:58 -0300

    Add memory and a doubled clock only to feed it

--- mipos.vhd
+++ mipos.vhd
@@ -10,13 +10,14 @@ use ieee.std_logic_unsigned.all;
 
 entity mipos is
 	port(
-		clk, rst: in std_logic
+		clk, clk_mem, rst: in std_logic
 	);
 end mipos;
 
 architecture beh of mipos is
 	signal curr_address: std_logic_vector(31 downto 0); -- Address of the instruction to run
 	signal next_address: std_logic_vector(31 downto 0); -- Next address to be loaded into PC
+	signal q_sig: std_logic_vector(31 downto 0); -- Next address to be loaded into PC
 	component PC
 		port (
 			clk, rst: in std_logic;
@@ -32,6 +33,14 @@ architecture beh of mipos is
 		);		
 	end component;
 	
+	component memory
+		port (
+			address		: IN STD_LOGIC_VECTOR (9 DOWNTO 0); 
+			clock		: IN STD_LOGIC  := '1';
+			q		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
+		);
+	end component;
+	
 begin
 	Prog: PC port map (clk, rst, next_address, curr_address); 
 
@@ -39,6 +48,14 @@ begin
 		a => curr_address,
 		b => "00000000000000000000000000000100",
 		c => next_address
+	);
+
+	memory_inst : memory port map (
+		-- the last two bits of the address are discarded so that the
+		-- +4 increment turns into a +1 increment to use the entire memory
+		address	 => curr_address(11 downto 2),
+		clock	 => clk_mem,
+		q	 => q_sig
 	);
 
 
--- mipos.vht
+++ mipos.vht
@@ -8,17 +8,19 @@ architecture mipos_arch of mipos_tst is
 -- constants
 constant T : time := 10 ns;
 -- signals                                                   
-signal clk : std_logic := '0';
+signal clk : std_logic := '0';                               
+signal clk_mem : std_logic := '0';
 signal rst : std_logic := '1';
 component mipos
 	port (
-	clk : in std_logic;
+	clk, clk_mem : in std_logic;
 	rst : in std_logic
 	);
 end component;
 begin
 	-- clock generation
 	clk <= not clk after T/2;
+	clk_mem <= not clk_mem after T/4;
 	
 	reset: process
 	begin
@@ -30,6 +32,7 @@ begin
 	
 	mipos_component : mipos port map(
 		clk => clk,
+		clk_mem => clk_mem,
 		rst => rst
 	);
   
