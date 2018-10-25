70493c8534ab8b8df0228b466304f3b76b507536
Eduardo Henke <eduardo_henke@hotmail.com>
2018-10-25 15:33:12 -0300

    Fix assembly code example

--- parser/main.py
+++ parser/main.py
@@ -3,17 +3,17 @@ from parser.io.file import to_mif
 
 print('Check your ROM.mif file')
 assembly = """
-addi $s3, $zero, 5 # i = 5
-addi $s4, $zero, 4 # j = 4
+addi $s3, $zero, 3  # i = 3
+addi $s4, $zero, 4  # j = 4
 
 addi $s1, $zero, 10 # b = 10
 addi $s2, $zero, 8  # c = 8
 
-slt $t0, $s3, $s4 # temp = a < j
-beq $t0, $zero, 2 # if(!temp)
-sub $s0, $s3, $s4 # then { a = b - c }
+slt $t0, $s3, $s4   # temp = i < j
+beq $t0, $zero, 2   # if(temp)
+sub $s0, $s1, $s2   # then { a = b - c }
 j 9 
-add $s0, $s1, $s2 # else { a = b + c }
+add $s0, $s1, $s2   # else { a = b + c }
 noop
 """
 
